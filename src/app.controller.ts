100a451c182e8882bfeb8db00cc4ee08db456a2f
오승은 <tmddms927@gmail.com>
2022-10-14 15:06:09 +0900

    [IN-72] add: API 설계 - 컬렉션 설계

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,53 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// 2-1
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
+
+// 1-1, 1-2, 1-3, 1-4
+@Controller("/login")
+export class Login {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
+
+// 2-2, 2-4
+@Controller("/users")
+export class Users {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
+
+// 3-1, 3-2
+@Controller("/games")
+export class Games {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
+
+// 4-0, 4-1, 4-2, 4-3
+@Controller("/chats")
+export class Chats {
   constructor(private readonly appService: AppService) {}
 
   @Get()
