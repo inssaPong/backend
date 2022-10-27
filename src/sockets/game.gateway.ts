e2de85a72857f389ddad5c15161364dbc0aed740
오승은 <tmddms927@gmail.com>
2022-10-27 16:39:05 +0900

    add: [IN-137][게임] 2명 접속 시 새로운 room으로 접속 - room_id : [player1 id]_[player2_id] - room 생성 후 해당 room안에 있는 클라이언트한테만 메세지 전송 성공

--- src/sockets/game.gateway.ts
+++ src/sockets/game.gateway.ts
@@ -8,24 +8,48 @@ import {
 import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Server, Socket } from 'socket.io';
+import { GameComponent, Player } from './game.component';
 
 // @WebSocketGateway({ namespace: 'game' }, { cors: true })
-@WebSocketGateway({ cors: true })
+@WebSocketGateway({ namespace: 'game', cors: true })
 export class GameGateway {
   @WebSocketServer()
   server: Server;
+  players: Player[] = [];
   private logger: Logger = new Logger('AppGateway');
+  // pp: GameComponent[];
 
-  afterInit(server: Server) {
-    this.logger.log('Init');
-  }
-
-  handleDisconnect(client: Socket) {
-    this.logger.log(`Client Disconnected : ${client.id}`);
-  }
+  afterInit() {}
 
   handleConnection(client: Socket, ...args: any[]) {
     this.logger.log(`Client Connected : ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.players = this.players.filter((player) => player.socket !== client);
+    this.logger.log(`Client Disconnected : ${client.id}`);
+  }
+
+  @SubscribeMessage('playingGame')
+  playingGame(client: Socket, id: string) {
+    if (this.players.find((element) => element.socket == client) == undefined) {
+      const new_player = new Player();
+      new_player.socket = client;
+      new_player.id = id;
+      this.logger.log(`${new_player.id}`);
+      this.players.push(new_player);
+    }
+    if (this.players.length > 1) {
+      this.logger.log(`2명 이상!!!!!! 룸 생성!!!!!!`);
+      let room_id: string;
+      room_id = this.players[0].id + '_' + this.players[1].id;
+      this.logger.log(`room id : ${room_id}`);
+      this.players[0].socket.join(room_id);
+      this.players[0].room_id = room_id;
+      this.players[1].socket.join(room_id);
+      this.players[1].room_id = room_id;
+      this.server.to(room_id).emit('joinRoom', room_id);
+    }
   }
 
   @SubscribeMessage('events')
@@ -36,8 +60,10 @@ export class GameGateway {
     );
   }
 
-  @SubscribeMessage('message')
+  @SubscribeMessage('move')
   handleMessage(client: Socket, data: string) {
-    client.broadcast.emit('message', data);
+    let player = this.players.find((element) => element.socket == client);
+    this.logger.log(`move events!!!!!!! : ${player.id}, ${player.room_id}`);
+    client.in(player.room_id).emit('changePostion', 'move!!!!!!!!');
   }
 }
