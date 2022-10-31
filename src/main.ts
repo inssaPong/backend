4904bb04dc3b61e22ec1bdeb54106789311c4bd5
오승은 <tmddms927@gmail.com>
2022-10-31 20:14:03 +0900

    fix: [IN-169] back front 로그인 기능 연결

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('트센 API 설계')
