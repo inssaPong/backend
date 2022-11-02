09d5d3286dd65bfd4a954b4f4a77436edbca2278
오승은 <tmddms927@gmail.com>
2022-11-02 09:11:41 +0900

    fix: games/schedule/updateBallPos 수정 - p1, p2 bar 먼저 확인 후 벽을 확인하게 변경

--- src/games/schedules/updateBallPos.serviec.ts
+++ src/games/schedules/updateBallPos.serviec.ts
@@ -6,24 +6,6 @@ export default function updateBallPos(
   gameRoom: GameRoomComponent,
   mainGateway: MainGateway,
 ) {
-  // 윗벽
-  if (gameRoom.ball_y <= 0 + GameObject.ball_radius) {
-    gameRoom.ball_y_dir = 1;
-  }
-  // 아래벽
-  if (gameRoom.ball_y > GameObject.canvas_height - GameObject.ball_radius) {
-    gameRoom.ball_y_dir = -1;
-  }
-  // 왼쪽벽
-  if (gameRoom.ball_x <= 0 + GameObject.ball_radius) {
-    gameRoom.p2_score++;
-    setTimeout(nextRound, 0, gameRoom, mainGateway);
-  }
-  // 오른쪽벽
-  if (gameRoom.ball_x > GameObject.canvas_width - GameObject.ball_radius) {
-    gameRoom.p1_score++;
-    setTimeout(nextRound, 0, gameRoom, mainGateway);
-  }
   // p1 bar
   if (
     gameRoom.ball_x - GameObject.ball_radius <=
@@ -34,12 +16,33 @@ export default function updateBallPos(
     gameRoom.ball_x_dir = 1;
   }
   // p2 bar
-  if (
+  else if (
     gameRoom.ball_x + GameObject.ball_radius >= gameRoom.p2_x &&
     gameRoom.ball_y > gameRoom.p2_y &&
     gameRoom.ball_y <= gameRoom.p2_y + GameObject.bar_height
   ) {
     gameRoom.ball_x_dir = -1;
+  }
+  // 윗벽
+  else if (gameRoom.ball_y <= 0 + GameObject.ball_radius) {
+    gameRoom.ball_y_dir = 1;
+  }
+  // 아래벽
+  else if (
+    gameRoom.ball_y >
+    GameObject.canvas_height - GameObject.ball_radius
+  ) {
+    gameRoom.ball_y_dir = -1;
+  }
+  // 왼쪽벽
+  else if (gameRoom.ball_x <= 0 + GameObject.ball_radius) {
+    gameRoom.p2_score++;
+    setTimeout(nextRound, 0, gameRoom, mainGateway);
+  }
+  // 오른쪽벽
+  else if (gameRoom.ball_x > GameObject.canvas_width - GameObject.ball_radius) {
+    gameRoom.p1_score++;
+    setTimeout(nextRound, 0, gameRoom, mainGateway);
   }
   gameRoom.ball_x += 1 * gameRoom.ball_x_dir;
   gameRoom.ball_y += 1 * gameRoom.ball_y_dir;
