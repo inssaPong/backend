3714958deda89e882ca6dd6abd7f1a4dccc97754
SangHwanJeon <rhaxxoddl@naver.com>
2022-11-05 17:42:05 +0900

    add: [IN-144] mypage repository 구현

--- src/mypage/mypage.repository.ts
+++ src/mypage/mypage.repository.ts
@@ -1,5 +1,8 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { property } from 'lodash';
 import { DatabaseService } from 'src/database/database.service';
+import { GameStatDto } from './dto/create-mypage.dto';
+import { UpdateUserInfoDto } from './dto/update-mypage.dto';
 
 @Injectable()
 export class MypageRepository {
@@ -15,30 +18,115 @@ export class MypageRepository {
 		WHERE id='${id}';
 		`,
       );
-      return;
+      this.logger.debug(`User Info: ${databaseResponse}`);
       if (databaseResponse.length <= 0) return '404 Not Found'; // TODO status code 뱉는 걸로 바꾸기
-      return databaseResponse;
+      return 400;
+    } catch (error) {
+      this.logger.error(`Error: ${error}`);
+      return 400;
+    }
+  }
+
+  async patchUserInfo(id: string, body: UpdateUserInfoDto) {
+    try {
+      let property: keyof typeof body;
+      let databaseResponse;
+      for (property in body) {
+        databaseResponse = await this.databaseService.runQuery(
+          `
+			  UPDATE "user"
+			  SET ${property} = '${body[property]}'
+			  WHERE id='${id}' AND ${property} = '${body[property]}';
+			`,
+        );
+        return 200;
+      }
+    } catch (error) {
+      this.logger.error(`Error: ${error}`);
+      return 500;
+    }
+  }
+
+  async patchTwoFactor(id: string, twofactor_status: boolean) {
+    try {
+      const databaseResponse = await this.databaseService.runQuery(
+        `
+			  UPDATE "user"
+			  SET twofactor_status = ${twofactor_status}
+			  WHERE id='${id}';
+			`,
+      );
     } catch (error) {
       this.logger.error(`Error: ${error}`);
       return 404;
     }
   }
 
-  async patchAvatar(id: string, avatar: string) {
+  async getFollows(id: string) {
     try {
       const databaseResponse = await this.databaseService.runQuery(
         `
-		UPDATE "user"
-		SET avatar = '${avatar}'
-		WHERE id='${id}';
+		SELECT partner_id
+		FROM "user_relation"
+		WHERE user_id='${id}' AND block_status=false;
 		`,
       );
-      this.logger.debug(`database response length: ${databaseResponse.length}`);
-      if (databaseResponse.length <= 0) return '404 Not Found'; // TODO status code 뱉는 걸로 바꾸기
+      this.logger.debug(`Follows length: ${databaseResponse.length}`);
+      this.logger.debug(`Follows: ${databaseResponse}`);
       return databaseResponse;
     } catch (error) {
       this.logger.error(`Error: ${error}`);
-      return 404;
+      return 400;
+    }
+  }
+
+  async getGameHistory(id: string) {
+    try {
+      const databaseResponse = await this.databaseService.runQuery(
+        `
+		SELECT winner_id, loser_id
+		FROM "game_history"
+		WHERE winner_id='${id}' OR loser_id='${id}';
+		`,
+      );
+      this.logger.debug(`GameHistory length: ${databaseResponse.length}`);
+      this.logger.debug(`GameHistory: ${databaseResponse}`);
+      return databaseResponse;
+    } catch (error) {
+      this.logger.error(`Error: ${error}`);
+      return 400;
+    }
+  }
+
+  async getWinHistory(id: string) {
+    try {
+      const wins = await this.databaseService.runQuery(
+        `
+		SELECT id
+		FROM "game_history"
+		WHERE winner_id='${id}';
+		`,
+      );
+      return wins;
+    } catch (error) {
+      this.logger.error(`Error: ${error}`);
+      return 400;
+    }
+  }
+
+  async getLoseHistory(id: string) {
+    try {
+      const loses = await this.databaseService.runQuery(
+        `
+		SELECT id
+		FROM "game_history"
+		WHERE loser_id='${id}';
+		`,
+      );
+      return loses;
+    } catch (error) {
+      this.logger.error(`Error: ${error}`);
+      return 400;
     }
   }
 }
