8b5882b404c07fc7065e20026aa3dc4a3f9d3994
오승은 <tmddms927@gmail.com>
2022-11-09 13:58:28 +0900

    add: [IN-193] 게임 결과 데이터 베이스에 저장

--- src/games/schedules/failSaveResult.ts
+++ src/games/schedules/failSaveResult.ts
@@ -4,4 +4,6 @@ import { GameGateway } from '../games.gateway';
 export default function failSaveResult(
   gameRoom: GameRoomComponent,
   gameGateway: GameGateway,
-) {}
+) {
+  gameGateway.mainGateway.server.to(gameRoom.room_id).emit('game/failSave');
+}
