6b7163a6c5391b5bf83d25aa5f94f9c5ae85d94e
오승은 <tmddms927@gmail.com>
2022-11-10 16:51:10 +0900

    add: [IN-196] 채널 입장시 이전 메세지 전송 & 차단한 사람 전송 제외

--- src/channels/channels.repository.ts
+++ src/channels/channels.repository.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class ChannelsRepository {
+  private readonly logger = new Logger(ChannelsRepository.name);
   constructor(private readonly databaseService: DatabaseService) {}
 
   async checkPossibleChannel(id: string, channel_id: string) {
@@ -10,12 +11,13 @@ export class ChannelsRepository {
       const databaseResponse = await this.databaseService.runQuery(
         `
           SELECT id FROM "channel_member"
-          WHERE id=${id} AND channel_id=${channel_id};
+          WHERE id='${id}' AND channel_id=${channel_id};
 		    `,
       );
       if (databaseResponse.length == 1) return 200;
       else return 400;
-    } catch (error) {
+    } catch (err) {
+      this.logger.log(`[checkPossibleChannel] ${err}`);
       return 500;
     }
   }
@@ -29,7 +31,8 @@ export class ChannelsRepository {
 		    `,
       );
       return databaseResponse;
-    } catch (error) {
+    } catch (err) {
+      this.logger.log(`[selectRoomMember] ${err}`);
       return databaseResponse;
     }
   }
@@ -43,8 +46,8 @@ export class ChannelsRepository {
 		    `,
       );
       return 201;
-    } catch (error) {
-      console.log('[ChannelDB]insertMessage : ' + error);
+    } catch (err) {
+      this.logger.log(`[insertMessage] ${err}`);
       return 500;
     }
   }
@@ -58,8 +61,43 @@ export class ChannelsRepository {
 		    `,
       );
       return 201;
-    } catch (error) {
-      console.log('[ChannelDB]insertDM : ' + error);
+    } catch (err) {
+      this.logger.log(`[insertDM] ${err}`);
+      return 500;
+    }
+  }
+
+  async selectAllMessage(channel_id: string) {
+    let databaseResponse: any[];
+    try {
+      databaseResponse = await this.databaseService.runQuery(
+        `
+          SELECT * FROM "message" WHERE channel_id=${channel_id};
+		    `,
+      );
+      return databaseResponse;
+    } catch (err) {
+      this.logger.log(`[selectAllMessage] ${err}`);
+      return databaseResponse;
+    }
+  }
+
+  async selectBlockUser(user_id: string, partner_id: string) {
+    try {
+      const databaseResponse = await this.databaseService.runQuery(
+        `
+          SELECT block_status FROM "user_relation"
+          WHERE user_id='${user_id}' AND partner_id='${partner_id}';
+		    `,
+      );
+      if (
+        databaseResponse.length == 1 &&
+        databaseResponse[0].block_status == true
+      )
+        return true;
+      else return false;
+    } catch (err) {
+      this.logger.log(`[selectBlockUser] ${err}`);
       return 500;
     }
   }
