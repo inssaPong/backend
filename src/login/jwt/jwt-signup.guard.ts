48c48e09d3c7eadf6ebacc5878dd06f8d81db074
Sondho <thseogh135@gmail.com>
2022-11-11 13:30:36 +0900

    refact: jwt-signup 구조 변경

--- src/login/jwt/jwt-signup.guard.ts
+++ src/login/jwt/jwt-signup.guard.ts
@@ -14,29 +14,47 @@ export class JwtSignGuard implements CanActivate {
     private readonly loginRepository: LoginRepository,
   ) {}
 
+  private readonly logger = new Logger(JwtSignGuard.name);
+
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const logger = new Logger(JwtSignGuard.name);
-    const request = context.switchToHttp().getRequest();
-    const response = context.switchToHttp().getResponse();
-    const user = request.user;
+    const req = context.switchToHttp().getRequest();
+    const res = context.switchToHttp().getResponse();
+
+    const user = req.user;
     if (user === undefined) {
-      logger.log('Undefined user');
+      this.logger.log('Undefined user');
       return false;
     }
-    const payload = { username: user.username, email: user.email };
-    const access_token = this.jwtService.sign(payload);
-    logger.debug(`access_token:  ${access_token}`);
-    response.cookie('Authorization', access_token, {
+    const access_token = this.jwtService.sign(user);
+    this.logger.debug(`access_token:  ${access_token}`);
+
+    res.cookie('Authorization', access_token, {
       // httpOnly: true, // TODO: true일때 보안은 좋으나 클라이언트에서 접근 불가. 어떻게 하지?
     });
-    const is_signup = await this.loginRepository.findUser(user.username);
-    if (is_signup === undefined) {
-      logger.log('User does not exist in DB.');
-      this.loginRepository.insertUser(user.username, user.username, user.email);
-      response.redirect('http://localhost:8080/editprofile');
-    } else {
-      logger.log('User is in DB.');
-      response.redirect('http://localhost:8080/home');
+
+    // Description: user 테이블에 해당 user가 존재하는지 여부 검사
+    try {
+      const has_user = await this.loginRepository.findUser(user.id);
+      if (has_user) {
+        this.logger.log('User is in DB.');
+        res.redirect('http://localhost:8080/home');
+        return res.status(200).send();
+      } else {
+        this.logger.log('User does not exist in DB.');
+      }
+    } catch (error) {
+      this.logger.error(error);
+      return res.status(500).send();
+    }
+
+    // Description: user 테이블에 insert
+    try {
+      await this.loginRepository.insertUser(user.id, user.id, user.email);
+      res.redirect('http://localhost:8080/editprofile');
+    } catch (error) {
+      this.logger.error(error);
+      res.redirect('http://localhost:8080');
+      return res.status(500).send();
     }
     return true;
   }
