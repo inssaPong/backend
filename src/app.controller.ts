d44bc22fa7dce51d7e07de9804cf0d9b53e58cdb
오승은 <tmddms927@gmail.com>
2022-11-15 16:50:56 +0900

    [IN-252] api 함수 내부에서 return이 문제. res.send()로 변경하면 잘 돌아감.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,7 +8,6 @@ export class AppController {
   private readonly logger: Logger = new Logger(AppController.name);
   constructor(private readonly appService: AppService) {}
 
-  // 게임 페이지 깡통 get()
   @Get('/loginCheck')
   loginCheckGet(@Req() req, @Res() res) {
     const user_id = req.user.id;
