88bbd835e200a14f4108e847266e849ade7f0875
SangHwanJeon <rhaxxoddl@naver.com>
2022-11-16 15:37:44 +0900

    add: 로그 찍는 함수
    - printObject: 객체 요소 로그 출력
    - printObjectArray: 객체 배열 요소 로그 출력
    - printStringArray: 문자열 배열 로그 출력 (mypageService에만 있음)

--- src/mypage/mypage.service.ts
+++ src/mypage/mypage.service.ts
@@ -3,19 +3,53 @@ import { Response } from 'express';
 
 @Injectable()
 export class MypageService {
-	errorHandler(error, res: Response, logger: Logger, functionName: string) {
-		if (error == 400) {
-		  logger.error(`${functionName}() Bad request return ${error}`);
-		  res.status(error).send();
-		} else if (error == 404) {
-		  logger.error(`${functionName}() Not found return ${error}`);
-		  res.status(error).send();
-		} else if (error == 500) {
-		  logger.error(`${functionName}() Database server error return ${error}`);
-		  res.status(error).send();
-		} else {
-		  logger.error(`${functionName}() Undefiend error: ${error}`);
-		  res.status(500).send();
-		}
-	  }
+  errorHandler(error, res: Response, logger: Logger, functionName: string) {
+    if (error == 400) {
+      logger.error(`${functionName}() Bad request return ${error}`);
+      res.status(error).send();
+    } else if (error == 404) {
+      logger.error(`${functionName}() Not found return ${error}`);
+      res.status(error).send();
+    } else if (error == 500) {
+      logger.error(`${functionName}() Database server error return ${error}`);
+      res.status(error).send();
+    } else {
+      logger.error(`${functionName}() Undefiend error: ${error}`);
+      res.status(500).send();
+    }
+  }
+
+  printObject(objectName: string, object: Object, logger: Logger) {
+    logger.debug(`print object <${objectName}> {`);
+    for (let [key, value] of Object.entries(object)) {
+      logger.debug(`	${key}: ${value}`);
+    }
+    logger.debug('}');
+  }
+
+  private printObjectArrayElement(object: Object, logger: Logger) {
+    for (let [key, value] of Object.entries(object)) {
+      logger.debug(`	${key}: ${value}, `);
+    }
+  }
+
+  printStringArray(name: string, stringArray: string[], logger: Logger) {
+    logger.debug(`print string array <${name}> [`);
+    for (let element of stringArray) {
+      logger.debug(`	${element}, `);
+    }
+    logger.debug(']');
+  }
+
+  printObjectArray(
+    objectName: string,
+    objectArray: Array<Object>,
+    logger: Logger,
+  ) {
+    logger.debug(`print object array <${objectName}> [`);
+    for (let element of objectArray) {
+      this.printObjectArrayElement(element, logger);
+    }
+    logger.debug(']');
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -18,4 +18,30 @@ export class UsersService {
       res.status(500).send();
     }
   }
+
+  printObject(objectName: string, object: Object, logger: Logger) {
+    logger.debug(`print object <${objectName}> {`);
+    for (let [key, value] of Object.entries(object)) {
+      logger.debug(`${key}: ${value}`);
+    }
+    logger.debug('}');
+  }
+
+  private printObjectArrayElement(object: Object, logger: Logger) {
+    for (let [key, value] of Object.entries(object)) {
+      logger.debug(`	${key}: ${value}, `);
+    }
+  }
+
+  printObjectArray(
+    objectName: string,
+    objectArray: Array<Object>,
+    logger: Logger,
+  ) {
+    logger.debug(`print object array <${objectName}> [`);
+    for (let element of objectArray) {
+      this.printObjectArrayElement(element, logger);
+    }
+    logger.debug(']');
+  }
 }
